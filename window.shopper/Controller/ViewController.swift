fc31ea7c4cfdfa7a376d4cb63cb48b00e29a2125
Tristan_Day <tmd526@gmail.com>
2017-09-06 13:48:38 -0500

    Slight UI Build

---
+++ window.shopper/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window.shopper
+//
+//  Created by Tristan Day on 9/6/17.
+//  Copyright © 2017 Tristan Day. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
